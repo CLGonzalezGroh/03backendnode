84a2701e627ec058653c01d3fecabfd71142cbde
Cesar Gonzalez Groh <clgonzalezgroh@hotmail.com>
2020-11-16 10:50:59 -0300

    final clase 22. Movies routes implementadas con Mocks

--- learning-node/so-archivos/os.js
+++ learning-node/so-archivos/os.js
@@ -5,7 +5,11 @@ console.log(
   "IP address",
   os.networkInterfaces().eth0.map((i) => i.address)
 );
-console.log("Free Memory:", os.freemem() / 1024 / 1024 / 1024, "Gb");
+console.log(
+  "Free Memory:",
+  (os.freemem() / 1024 / 1024 / 1024).toFixed(2),
+  "Gb"
+);
 console.log("OS Type:", os.type());
 console.log("OS Version:", os.release());
 console.log("User Info:", os.userInfo());
