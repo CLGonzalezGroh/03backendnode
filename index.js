f25dd4f981bbd91ee4031c80481c1c95cb35806e
Cesar Gonzalez Groh <clgonzalezgroh@hotmail.com>
2020-11-17 21:08:44 -0300

    Final clase 35. Validando schema de la peticion con Joi y mensajes con Boom

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const express = require("express");
 const app = express();
 const { config } = require("./config/index");
 const moviesApi = require("./routes/movies");
+const {
+  logErrors,
+  wrapErrors,
+  errorHandler,
+} = require("./utils/middleware/errorHandler");
+const notFoundHandler = require("./utils/middleware/notFoundHandler");
 
+//body parser
 app.use(express.json());
 
+//routes
 moviesApi(app);
+//Not found
+app.use(notFoundHandler);
+
+// errors middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,11 @@
 const MovieServices = require("../services/movies");
 const express = require("express");
+const {
+  movieIdSchema,
+  createMovieSchema,
+  updateMovieSchema,
+} = require("../utils/schemas/movies");
+const validationHandler = require("../utils/middleware/validationHandler");
 
 function moviesApi(app) {
   const router = express.Router();
@@ -19,19 +25,27 @@ function moviesApi(app) {
       next(err);
     }
   });
-  router.get("/:movieId", async function (req, res, next) {
-    const { movieId } = req.params;
-    try {
-      const movies = await movieServices.getMovie({ movieId });
-      res.status(200).json({
-        data: movies,
-        message: "movie retrieved",
-      });
-    } catch (err) {
-      next(err);
+  router.get(
+    "/:movieId",
+    validationHandler({ movieId: movieIdSchema }, "params"),
+    async function (req, res, next) {
+      const { movieId } = req.params;
+      try {
+        const movies = await movieServices.getMovie({ movieId });
+        res.status(200).json({
+          data: movies,
+          message: "movie retrieved",
+        });
+      } catch (err) {
+        next(err);
+      }
     }
-  });
-  router.post("/", async function (req, res, next) {
+  );
+  router.post("/", validationHandler(createMovieSchema), async function (
+    req,
+    res,
+    next
+  ) {
     const { body: movie } = req;
     try {
       const createdMovieId = await movieServices.createMovie({ movie });
@@ -43,34 +57,43 @@ function moviesApi(app) {
       next(err);
     }
   });
-  router.put("/:movieId", async function (req, res, next) {
-    const { movieId } = req.params;
-    const { body: movie } = req;
-    try {
-      const updatedMovieId = await movieServices.updateMovie({
-        movieId,
-        movie,
-      });
-      res.status(200).json({
-        data: updatedMovieId,
-        message: "movie updated",
-      });
-    } catch (err) {
-      next(err);
+  router.put(
+    "/:movieId",
+    validationHandler({ movieId: movieIdSchema }, "params"),
+    validationHandler(updateMovieSchema),
+    async function (req, res, next) {
+      const { movieId } = req.params;
+      const { body: movie } = req;
+      try {
+        const updatedMovieId = await movieServices.updateMovie({
+          movieId,
+          movie,
+        });
+        res.status(200).json({
+          data: updatedMovieId,
+          message: "movie updated",
+        });
+      } catch (err) {
+        next(err);
+      }
     }
-  });
-  router.delete("/:movieId", async function (req, res, next) {
-    const { movieId } = req.params;
-    try {
-      const deletedMovieId = await movieServices.deleteMovie({ movieId });
-      res.status(200).json({
-        data: deletedMovieId,
-        message: "movie deleted",
-      });
-    } catch (err) {
-      next(err);
+  );
+  router.delete(
+    "/:movieId",
+    validationHandler({ movieId: movieIdSchema }, "params"),
+    async function (req, res, next) {
+      const { movieId } = req.params;
+      try {
+        const deletedMovieId = await movieServices.deleteMovie({ movieId });
+        res.status(200).json({
+          data: deletedMovieId,
+          message: "movie deleted",
+        });
+      } catch (err) {
+        next(err);
+      }
     }
-  });
+  );
 }
 
 module.exports = moviesApi;
