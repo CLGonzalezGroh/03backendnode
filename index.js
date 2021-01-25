98724d70192403f2443614543d05796b9092c9ed
Cesar Gonzalez Groh <clgonzalezgroh@hotmail.com>
2021-01-25 20:47:59 -0300

    Creado la ruta userMovies

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const express = require("express");
-const app = express();
-const { config } = require("./config/index");
-const moviesApi = require("./routes/movies");
+const express = require("express")
+const app = express()
+const { config } = require("./config/index")
+const moviesApi = require("./routes/movies")
+const userMoviesApi = require("./routes/userMovies")
 const {
   logErrors,
   wrapErrors,
   errorHandler,
-} = require("./utils/middleware/errorHandler");
-const notFoundHandler = require("./utils/middleware/notFoundHandler");
+} = require("./utils/middleware/errorHandler")
+const notFoundHandler = require("./utils/middleware/notFoundHandler")
 
 //body parser
-app.use(express.json());
+app.use(express.json())
 
 //routes
-moviesApi(app);
+moviesApi(app)
+userMoviesApi(app)
 //Not found
-app.use(notFoundHandler);
+app.use(notFoundHandler)
 
 // errors middleware
-app.use(logErrors);
-app.use(wrapErrors);
-app.use(errorHandler);
+app.use(logErrors)
+app.use(wrapErrors)
+app.use(errorHandler)
 
 app.listen(config.port, function () {
-  console.log(`Listening http://localhost:${config.port}`);
-});
+  console.log(`Listening http://localhost:${config.port}`)
+})
