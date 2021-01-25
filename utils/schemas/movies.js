e823e2dd1eabf22a7632df4278ac89e8a5a0fab8
Cesar Gonzalez Groh <clgonzalezgroh@hotmail.com>
2021-01-25 10:00:18 -0300

    Implementando Usuarios para el curso de Passport

--- utils/schemas/movies.js
+++ utils/schemas/movies.js
@@ -1,14 +1,14 @@
-const Joi = require("joi");
+const joi = require("joi")
 
-const movieIdSchema = Joi.string().regex(/^[0-9a-fA-F]{24}$/);
-const movieTitleSchema = Joi.string().max(80);
-const movieYearSchema = Joi.number().min(1888).max(2077);
-const movieCoverSchema = Joi.string().uri();
-const movieDescriptionSchema = Joi.string().max(300);
-const movieDurationSchema = Joi.number().min(1).max(300);
-const movieContentRatingSchema = Joi.string().max(5);
-const movieSourceSchema = Joi.string().uri();
-const movieTagsSchema = Joi.array().items(Joi.string().max(50));
+const movieIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/)
+const movieTitleSchema = joi.string().max(80)
+const movieYearSchema = joi.number().min(1888).max(2077)
+const movieCoverSchema = joi.string().uri()
+const movieDescriptionSchema = joi.string().max(300)
+const movieDurationSchema = joi.number().min(1).max(300)
+const movieContentRatingSchema = joi.string().max(5)
+const movieSourceSchema = joi.string().uri()
+const movieTagsSchema = joi.array().items(joi.string().max(50))
 
 const createMovieSchema = {
   title: movieTitleSchema.required(),
@@ -19,7 +19,7 @@ const createMovieSchema = {
   contentRating: movieContentRatingSchema.required(),
   source: movieSourceSchema.required(),
   tags: movieTagsSchema,
-};
+}
 
 const updateMovieSchema = {
   title: movieTitleSchema,
@@ -30,10 +30,10 @@ const updateMovieSchema = {
   contentRating: movieContentRatingSchema,
   source: movieSourceSchema,
   tags: movieTagsSchema,
-};
+}
 
 module.exports = {
   movieIdSchema,
   createMovieSchema,
   updateMovieSchema,
-};
+}
